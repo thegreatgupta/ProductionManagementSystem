cebe3e6a0ee3051d82efd4521191b68fb52bffa2
thegreatgupta <chandra.prakashg01@gmail.com>
2016-07-06 14:22:39 +0530

    LAN URL Feeded.

--- app/src/main/java/com/jeevani/productionmanagementsystem/constant/Constant.java
+++ app/src/main/java/com/jeevani/productionmanagementsystem/constant/Constant.java
@@ -6,7 +6,8 @@ package com.jeevani.productionmanagementsystem.constant;
 public class Constant {
 
     public static final String IP_ADDRESS = "" +
-            "http://localhost:8088/"+
+            "http://192.168.0.6:8080/"+
+            //"http://localhost:8088/"+
             "PMS/api/";
             //"http://192.168.0.109:8080/" +
             //"com.coginnotech.dailysteps/services/DailyStepsServices/";
